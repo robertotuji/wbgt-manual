bf5d56a88a11304b7d9430761bd1ef3701681325
robertotuji <robertotuji@gmail.com>
2025-06-18 20:32:31 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "wbgt-manual-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./manifest.json",
+  "./style.css",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Instalação do Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativação do Service Worker
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requisições
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
